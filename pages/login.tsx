208ca0c21504d34360f3101f427a439ff09c7355
Horváth Áron <hor.aaron@gmail.com>
2019-10-20 21:06:57 +0200

    TsLint error fixed. Login and logout is functional

--- pages/login.tsx
+++ pages/login.tsx
@@ -22,38 +22,60 @@ function LoginPage() {
     <div>
       <div>
         <h2>Auth</h2>
-        {!isLoaded(auth) ? (
-          <span>Loading...</span>
-        ) : isEmpty(auth) ? (
-          <StyledDiv>
-            <button type="button" onClick={loginWithGoogle}>
-              Login With Google
-            </button>
-            <button type="button" onClick={signUpWithPassword}>
-              Sign Up With Password
-            </button>
-            <button type="button" onClick={signInWithPassword}>
-              Sign In With Password
-            </button>
-          </StyledDiv>
-        ) : (
-          <StyledDiv>
-            <button type="button" onClick={logOut}>
-              Logout
-            </button>
-            <pre>{JSON.stringify(auth, null, 2)}</pre>
-          </StyledDiv>
-        )}
+        {(() => {
+          if (!isLoaded(auth)) return <span>Loading...</span>;
+          if (isLoaded(auth) && !isEmpty(auth))
+            return (
+              <StyledDiv>
+                <StyledButton type="button" onClick={logOut}>
+                  Logout
+                </StyledButton>
+                <pre>{JSON.stringify(auth, null, 2)}</pre>
+              </StyledDiv>
+            );
+          return (
+            <StyledDiv>
+              <StyledButton type="button" onClick={loginWithGoogle}>
+                Login With Google
+              </StyledButton>
+              <StyledButton type="button" onClick={signUpWithPassword}>
+                Sign Up With Password
+              </StyledButton>
+              <StyledButton type="button" onClick={signInWithPassword}>
+                Sign In With Password
+              </StyledButton>
+            </StyledDiv>
+          );
+        })()}
       </div>
     </div>
   );
 }
 const StyledDiv = styled.div`
   border: 1px solid #018dc4;
-  width: 50%;
+  width: 80%;
   background: gray;
   margin: auto;
   padding: 10px;
 `;
-
+const StyledButton = styled.button`
+  padding: 10px 20px;
+  border: 1px solid #018dc4;
+  border-radius: 3px;
+  font: ${props => props.theme.font};
+  color: rgba(86, 86, 86, 0.9);
+  background: #ffc107;
+  box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.2);
+  text-shadow: -1px -1px 0 rgba(15, 73, 168, 0.66);
+`;
+const StyledInput = styled.input`
+  padding: 10px 20px;
+  display: inline;
+  border: 1px solid #b7b7b7;
+  font: ${props => props.theme.font};
+  color: rgba(86, 86, 86, 0.9);
+  background: rgba(252, 252, 252, 1);
+  box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.2) inset;
+  text-shadow: 1px 1px 0 rgba(255, 255, 255, 0.66);
+`;
 export default LoginPage;
