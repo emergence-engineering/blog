1469822ed3206e99afef59fb02cfec8bee913784
Horváth Áron <hor.aaron@gmail.com>
2019-10-20 19:25:53 +0200

    Added logout button if logged in

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useSelector } from "react-redux";
 import { useFirebase, isLoaded, isEmpty } from "react-redux-firebase";
+import styled from "styled-components";
 import { RootState } from "../setup/reducers/rootReducer";
 
 function LoginPage() {
   const firebase = useFirebase();
   const auth = useSelector((state: RootState) => state.firebase.auth);
 
-  function loginWithGoogle() {
-    return firebase.login({ provider: "google", type: "popup" });
-  }
+  const loginWithGoogle = useCallback((e: any) => {
+    firebase.login({ provider: "google", type: "popup" });
+  }, []);
+
+  const logOut = useCallback((e: any) => {}, []);
 
   return (
     <div>
@@ -22,11 +25,23 @@ function LoginPage() {
             Login With Google
           </button>
         ) : (
-          <pre>{JSON.stringify(auth, null, 2)}</pre>
+          <StyledDiv>
+            <button type="button" onClick={logOut}>
+              Logout
+            </button>
+            <pre>{JSON.stringify(auth, null, 2)}</pre>
+          </StyledDiv>
         )}
       </div>
     </div>
   );
 }
+const StyledDiv = styled.div`
+  border: 1px solid #018dc4;
+  width: 50%;
+  background: gray;
+  margin: auto;
+  padding: 10px;
+`;
 
 export default LoginPage;
