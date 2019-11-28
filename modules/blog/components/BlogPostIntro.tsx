11def302c4c43e4f346350cb85aa7435761e6b58
blaisehorvath <blaisehorvath@gmail.com>
2019-11-28 10:01:26 +0100

    Added first article

--- modules/blog/components/BlogPostIntro.tsx
+++ modules/blog/components/BlogPostIntro.tsx
@@ -4,12 +4,8 @@ import Link from "next/link";
 
 import { convertTimestampToLocaleDateString } from "../../../utils/time";
 import AsyncSVGIcon from "../../common/components/AsyncSvgIcon";
-
-const Description = styled.p`
-  font-weight: lighter;
-  text-align: justify;
-  text-justify: distribute;
-`;
+import Markdown from "../../article/components/Markdown";
+import { ArticleIntro } from "../../../types/article";
 
 const ReadMoreLink = styled.a`
   text-decoration: none;
@@ -21,26 +17,8 @@ const PostMetadataContainer = styled.div``;
 
 const Root = styled.div``;
 
-interface BlogPostIntro {
-  title: string;
-  introText: string;
-  author: string;
-  authorLink: string | null;
-  postId: string;
-  timestamp: number;
-}
-
-const content: BlogPostIntro = {
-  title: "Post title",
-  author: "Balázs",
-  authorLink: null,
-  introText: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`,
-  postId: "article-1",
-  timestamp: 1573478828692,
-};
-
-const BlogPostIntro: FunctionComponent<{}> = () => {
-  const { introText, title, postId, author, timestamp } = content;
+const BlogPostIntro: FunctionComponent<ArticleIntro> = props => {
+  const { introText, title, postId, author, timestamp } = props;
 
   const postDate = convertTimestampToLocaleDateString(timestamp);
   return (
@@ -52,7 +30,7 @@ const BlogPostIntro: FunctionComponent<{}> = () => {
           {postDate}
         </span>
       </PostMetadataContainer>
-      <Description>{introText}</Description>
+      <Markdown source={introText} />
       <Link href={`/blog/${postId}`}>
         <ReadMoreLink>Read more...</ReadMoreLink>
       </Link>
---
+++ types/article.ts
@@ -0,0 +1,8 @@
+export interface ArticleIntro {
+  title: string;
+  introText: string;
+  author: string;
+  authorLink: string | null;
+  postId: string;
+  timestamp: number;
+}
