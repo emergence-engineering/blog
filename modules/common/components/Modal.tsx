24b0edf23de2d0470de1b3c1ba3a9291ed4992be
blaisehorvath <blaisehorvath@gmail.com>
2019-12-08 12:02:16 +0100

    Modal changes

--- modules/common/components/Modal.tsx
+++ modules/common/components/Modal.tsx
@@ -4,23 +4,23 @@ import styled from "styled-components";
 import theme from "../../../utils/theme";
 
 const Root = styled.div`
-  position: absolute;
-  z-index: 2;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 100;
   background-color: rgba(0, 0, 0, 0.2);
   width: 100%;
   height: 100vh;
-  top: 0;
-  left: 0;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const ModalWrapper = styled.div`
-  width: 40%;
+  width: 55%;
   min-height: 20rem;
   background-color: ${theme.color.gray10};
-  z-index: 3;
+  z-index: 101;
   border-radius: 0.5em;
   opacity: 1;
 `;
