613f4081ef80925cf0053cdeeb19a952880e9505
blaisehorvath <blaisehorvath@gmail.com>
2020-01-10 15:44:32 +0100

    Removed 3rd party blog links temporarily

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,10 +4,10 @@ import styled from "styled-components";
 import Layout from "../../modules/common/components/Layout";
 import theme, { screenSizes, sizes } from "../../utils/theme";
 import BlogPostIntro from "../../modules/blog/components/BlogPostIntro";
-import {
-  CommunityBlogItem,
-  CommunityBlogs,
-} from "../../modules/blog/components/CommunityBlogSite";
+// import {
+//   CommunityBlogItem,
+//   CommunityBlogs,
+// } from "../../modules/blog/components/CommunityBlogSite";
 
 import { article1Metadata } from "./everyday-javascript-1-spread-operator";
 
@@ -31,39 +31,38 @@ const ContentWrapper = styled.div`
   flex-direction: column;
 `;
 
-const CommunityBlogRoot = styled.div`
-  padding: 0.5rem 2rem;
-  border-radius: 0.2rem;
-  border-style: solid;
-  border-color: ${theme.color.gray7};
-  border-width: thin;
-  margin-top: 4rem;
-`;
+// const CommunityBlogRoot = styled.div`
+//   padding: 0.5rem 2rem;
+//   border-radius: 0.2rem;
+//   border-style: solid;
+//   border-color: ${theme.color.gray7};
+//   border-width: thin;
+//   margin-top: 4rem;
+// `;
 
-const CommunityBlogSection: FunctionComponent<{}> = () => (
-  <CommunityBlogRoot>
-    <h1>Follow us on community blog sites as well</h1>
-    <CommunityBlogs>
-      <CommunityBlogItem
-        faIconName="fab fa-medium"
-        caption="On Medium.com"
-        href="https://medium.com/@emergence_engineering"
-      />
-      <CommunityBlogItem
-        faIconName="fab fa-dev"
-        caption="On Dev.to"
-        href="https://dev.to/emergence"
-      />
-    </CommunityBlogs>
-  </CommunityBlogRoot>
-);
+// const CommunityBlogSection: FunctionComponent<{}> = () => (
+//   <CommunityBlogRoot>
+//     <h1>Follow us on community blog sites as well</h1>
+//     <CommunityBlogs>
+//       <CommunityBlogItem
+//         faIconName="fab fa-medium"
+//         caption="On Medium.com"
+//         href="https://medium.com/@emergence_engineering"
+//       />
+//       <CommunityBlogItem
+//         faIconName="fab fa-dev"
+//         caption="On Dev.to"
+//         href="https://dev.to/emergence"
+//       />
+//     </CommunityBlogs>
+//   </CommunityBlogRoot>
+// );
 
 const Blog: FunctionComponent<{}> = () => (
   <Layout>
     <Root>
       <ContentWrapper>
         <BlogPostIntro {...article1Metadata} />
-        <CommunityBlogSection />
       </ContentWrapper>
     </Root>
   </Layout>
