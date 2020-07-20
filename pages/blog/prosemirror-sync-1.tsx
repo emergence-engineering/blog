ccd2df718c9976d33ed60caa53e810aa38975cf3
Viktor Vaczi <viktor.vaczi.90@gmail.com>
2020-07-20 20:08:31 +0200

    Syncing works, versioning issues.

--- pages/blog/prosemirror-sync-1.tsx
+++ pages/blog/prosemirror-sync-1.tsx
@@ -6,7 +6,12 @@ import { exampleSetup } from "prosemirror-example-setup";
 import { Schema } from "prosemirror-model";
 import PouchDB from "pouchdb";
 import styled from "styled-components";
-import { collab, sendableSteps } from "prosemirror-collab";
+import {
+  collab,
+  getVersion,
+  receiveTransaction,
+  sendableSteps,
+} from "prosemirror-collab";
 import { Step } from "prosemirror-transform";
 
 import ArticleWrapper from "../../modules/article/components/ArticleWrapper";
@@ -131,6 +136,8 @@ const Article: FunctionComponent<{}> = () => {
       clientDB2.sync(serverDB, { live: true });
 
       serverDB.setMaxListeners(20);
+      clientDB1.setMaxListeners(20);
+      clientDB2.setMaxListeners(20);
 
       setDBS({
         serverDB,
@@ -157,26 +164,62 @@ const Article: FunctionComponent<{}> = () => {
       DBS.serverDB.put(doc);
     })();
   }, [DBS]);
-  // useEffect(() => {
-  //   (async () => {
-  //     if (!DBS || !pmState2) return;
-  //     const listener = DBS.clientDB1.changes({
-  //       since: "now",
-  //       live: true,
-  //       // eslint-disable-next-line @typescript-eslint/camelcase
-  //       include_docs: true,
-  //       // eslint-disable-next-line @typescript-eslint/camelcase
-  //       filter: data =>
-  //         data.collection === DBCollection.ServerSteps &&
-  //         (data as ),
-  //     });
-  //     setDocListener(listener);
-  //     listener.on("change", data => {
-  //       // TODO: Don't listen to it after it is fetched
-  //       setServerDoc(data.doc as any);
-  //     });
-  //   })();
-  // }, [DBS, pmState2]);
+  // Fetching steps for view 2
+  useEffect(() => {
+    (async () => {
+      if (!DBS || !pmView2) return;
+      const listener = DBS.clientDB2.changes({
+        since: "now",
+        live: true,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        include_docs: true,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        filter: data =>
+          data.collection === DBCollection.ServerSteps &&
+          (data as ServerStep).docId === "ee" &&
+          (data as ServerStep).version === getVersion(pmView2.state),
+      });
+      listener.on("change", data => {
+        const serverStep: ServerStep = data.doc as any;
+        pmView2.dispatch(
+          receiveTransaction(
+            pmView2.state,
+            [Step.fromJSON(mySchema, serverStep.step)],
+            [serverStep.pmViewId],
+          ),
+        );
+      });
+    })();
+  }, [DBS, pmView2]);
+
+  // Fething steps for view 1
+  // TODO: Same as the one above
+  useEffect(() => {
+    (async () => {
+      if (!DBS || !pmView1) return;
+      const listener = DBS.clientDB1.changes({
+        since: "now",
+        live: true,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        include_docs: true,
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        filter: data =>
+          data.collection === DBCollection.ServerSteps &&
+          (data as ServerStep).docId === "ee" &&
+          (data as ServerStep).version === getVersion(pmView1.state),
+      });
+      listener.on("change", data => {
+        const serverStep: ServerStep = data.doc as any;
+        pmView1.dispatch(
+          receiveTransaction(
+            pmView1.state,
+            [Step.fromJSON(mySchema, serverStep.step)],
+            [serverStep.pmViewId],
+          ),
+        );
+      });
+    })();
+  }, [DBS, pmView1]);
 
   useEffect(() => {
     (async () => {
@@ -243,9 +286,28 @@ const Article: FunctionComponent<{}> = () => {
     setPmView1(view1);
     const view2 = new EditorView(document.querySelector("#editor2"), {
       state: state2,
+      dispatchTransaction: tr => {
+        const newState = view2.state.apply(tr);
+        setPmState2(newState);
+        view2.updateState(newState);
+        const sendable = sendableSteps(newState);
+        if (sendable && DBS) {
+          console.log({ sendable });
+          const newStep: ClientStep = {
+            steps: sendable.steps.map(step => step.toJSON()),
+            version: sendable.version,
+            status: StepStatus.NEW,
+            collection: DBCollection.ClientSteps,
+            docId: "ee", // TODO
+            pmViewId: sendable.clientID,
+          };
+          DBS.clientDB2.post(newStep);
+        }
+      },
     });
     setPmView2(view2);
   }, [serverDoc]);
+
   // Server listener
   useEffect(() => {
     (async () => {
@@ -261,39 +323,50 @@ const Article: FunctionComponent<{}> = () => {
             data.status === StepStatus.NEW,
         })
         .on("change", async data => {
-          const clientStep: ClientStep = data.doc as any;
-          const syncDoc: PMDocument = (await DBS.serverDB.get(clientStep.docId)) as any;
-          if (clientStep.version !== syncDoc.version) {
-            // TODO: Set status to StepStatus.REJECTED
-            return;
-          }
-          let doc = mySchema.nodeFromJSON(syncDoc.doc);
-          const { steps } = clientStep;
-          // TODO: Reduce BALAZS!!!
-          for (let i = 0; i < steps.length; i += 1) {
-            const result = Step.fromJSON(mySchema, steps[i]).apply(doc);
-            doc = result.doc;
-          }
+          try {
+            const clientStep: ClientStep = data.doc as any;
+            const syncDoc: PMDocument = (await DBS.serverDB.get(
+              clientStep.docId,
+            )) as any;
+            if (clientStep.version !== syncDoc.version) {
+              // TODO: Set status to StepStatus.REJECTED
+              return;
+            }
+            let doc = mySchema.nodeFromJSON(syncDoc.doc);
+            const { steps } = clientStep;
+            // TODO: Reduce BALAZS!!!
+            for (let i = 0; i < steps.length; i += 1) {
+              const result = Step.fromJSON(mySchema, steps[i]).apply(doc);
+              doc = result.doc;
+            }
 
-          const newVersion = syncDoc.version + steps.length;
-          const serverSteps: ServerStep[] = steps.map((step, index) => ({
-            collection: DBCollection.ServerSteps,
-            step,
-            pmViewId: clientStep.pmViewId,
-            version: syncDoc.version + index,
-            docId: syncDoc._id
-          }));
-          const newDoc: PMDocument = {
-            ...syncDoc,
-            version: newVersion,
-            doc: doc.toJSON(),
-            // eslint-disable-next-line no-underscore-dangle
-            _rev: syncDoc._rev,
-            updatedAt: Date.now().toString(),
-          };
-          await DBS.serverDB.bulkDocs(serverSteps);
-          await DBS.serverDB.put(newDoc);
-          console.log({ clientStep, doc });
+            const newVersion = syncDoc.version + steps.length;
+            const serverSteps: ServerStep[] = steps.map((step, index) => ({
+              collection: DBCollection.ServerSteps,
+              step,
+              pmViewId: clientStep.pmViewId,
+              version: syncDoc.version + index,
+              docId: syncDoc._id,
+            }));
+            const newDoc: PMDocument = {
+              ...syncDoc,
+              version: newVersion,
+              doc: doc.toJSON(),
+              // eslint-disable-next-line no-underscore-dangle
+              _rev: syncDoc._rev,
+              updatedAt: Date.now().toString(),
+            };
+            console.log("1", doc.textContent);
+            await DBS.serverDB
+              .put(newDoc)
+              .then(() => DBS.serverDB.bulkDocs(serverSteps));
+            console.log("2", doc.textContent);
+            console.log({ clientStep, doc });
+          } catch (e) {
+            // TODO: Set status to StepStatus.REJECTED
+            console.log(e);
+            // TODO
+          }
         });
     })();
   }, [DBS]);
