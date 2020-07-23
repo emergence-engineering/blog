779561c60464b83ae1462a9295ca8783863b7b56
blaisehorvath <blaisehorvath@gmail.com>
2020-07-23 08:49:35 +0200

    Updated next config: release_id, commit_id, firebase emulator, pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /* eslint-disable */
 require("dotenv").config();
+const {name} = require("./package.json")
 
+const gitCommitId = require('git-commit-id')
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
+const withCSS = require("@zeit/next-css");
+const withPlugins = require("next-compose-plugins");
+const withOptimizedImages = require("next-optimized-images");
+const withPwa = require("next-pwa")
 
-module.exports = {
+
+function getReleaseId(environment, commitId, appName) {
+  return `${appName}-${environment}-${commitId}`
+}
+
+const COMMIT_ID = gitCommitId()
+
+const nextConfig = {
   webpack: config => {
     config.plugins = config.plugins || [];
 
@@ -31,5 +44,19 @@ module.exports = {
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_DATABASE_NAME: process.env.FIREBASE_DATABASE_NAME,
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_SENDER_ID: process.env.FIREBASE_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FUNCTIONS_EMULATOR_HOST: process.env.FUNCTIONS_EMULATOR_HOST,
+    FIRESTORE_EMULATOR_HOST: process.env.FIRESTORE_EMULATOR_HOST,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    NODE_ENV: process.env.NODE_ENV,
+    COMMIT_ID,
+    RELEASE_ID: getReleaseId(process.env.NODE_ENV, COMMIT_ID, name)
   },
+  pwa: {
+    dest: "public"
+  }
 };
+
+module.exports = withPlugins([withCSS, withOptimizedImages, withPwa], nextConfig);
+
