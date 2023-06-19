c733fe4bcbe66f6913b904bc3f4e51f972e2b927
Norbert89 <norbert@emergence-engineering.com>
2023-06-19 17:27:56 +0200

    Removed Peter and embedded section

--- features/landingPage/components/WhatWeDo.tsx
+++ features/landingPage/components/WhatWeDo.tsx
@@ -2,8 +2,11 @@ import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 
 import theme, { sizes } from "../../../utils/theme";
-import {CapabilityBoxContainer, CapabilityBoxContainerWrapper} from "./Capability";
-import CapabilityBox, {CapabilityImages} from "./CapabilityBox";
+import {
+  CapabilityBoxContainer,
+  CapabilityBoxContainerWrapper,
+} from "./Capability";
+import CapabilityBox, { CapabilityImages } from "./CapabilityBox";
 
 const Root = styled.div`
   display: flex;
@@ -31,65 +34,58 @@ export const FacePictureWrapper = styled.div`
 `;
 
 const CapabilityBoxes = [
-    {
-        title: "ProseMirror",
-        content: `Modern web based rich text editors, full of crazy features`,
-        iconSrc: CapabilityImages.ProsemirrorImg,
-    },
-    {
-        title: "TipTap",
-        content: `Headless, TypeScript based out of the box collaborative rich text editor with ProseMirror under the hood`,
-        iconSrc: CapabilityImages.TipTapImg,
-    },
-    {
-        title: "Collaborative UIs",
-        content: `Real time collaboration for your web app with Yjs, Hocuspocus, GraphQL or FireBase`,
-        iconSrc: CapabilityImages.GroupImg,
-    },
-    {
-        title: "React",
-        content: `The standard of current front-ends`,
-        iconSrc: CapabilityImages.ReactImg,
-    },
-    {
-        title: "Scala",
-        content: `If you want a robust backend that scales easily.`,
-        iconSrc: CapabilityImages.ScalaImg,
-    },
-    {
-        title: "Cloud",
-        content: `Experience with all of the major cloud providers and some more.`,
-        iconSrc: CapabilityImages.CloudImg,
-    },
-    {
-        title: "DevOps",
-        content: `From CI & CD to Kubernetes.`,
-        iconSrc: CapabilityImages.KubernetesImg,
-    },
-    {
-        title: "Embedded",
-        content: `
-       From debugging PCBs to writing custom firmware.`,
-        iconSrc: CapabilityImages.MicrochipImg,
-    },
+  {
+    title: "ProseMirror",
+    content: `Modern web based rich text editors, full of crazy features`,
+    iconSrc: CapabilityImages.ProsemirrorImg,
+  },
+  {
+    title: "TipTap",
+    content: `Headless, TypeScript based out of the box collaborative rich text editor with ProseMirror under the hood`,
+    iconSrc: CapabilityImages.TipTapImg,
+  },
+  {
+    title: "Collaborative UIs",
+    content: `Real time collaboration for your web app with Yjs, Hocuspocus, GraphQL or FireBase`,
+    iconSrc: CapabilityImages.GroupImg,
+  },
+  {
+    title: "React",
+    content: `The standard of current front-ends`,
+    iconSrc: CapabilityImages.ReactImg,
+  },
+  {
+    title: "Scala",
+    content: `If you want a robust backend that scales easily.`,
+    iconSrc: CapabilityImages.ScalaImg,
+  },
+  {
+    title: "Cloud",
+    content: `Experience with all of the major cloud providers and some more.`,
+    iconSrc: CapabilityImages.CloudImg,
+  },
+  {
+    title: "DevOps",
+    content: `From CI & CD to Kubernetes.`,
+    iconSrc: CapabilityImages.KubernetesImg,
+  },
 ];
-
 
 const WhatWeDoSection: FunctionComponent = () => (
   <Root>
     <SectionTitle>What We do</SectionTitle>
-      <CapabilityBoxContainerWrapper>
-          <CapabilityBoxContainer>
-              {CapabilityBoxes.map(({ title, content, iconSrc }) => (
-                  <CapabilityBox
-                      title={title}
-                      content={content}
-                      iconSrc={iconSrc}
-                      key={title}
-                  />
-              ))}
-          </CapabilityBoxContainer>
-      </CapabilityBoxContainerWrapper>
+    <CapabilityBoxContainerWrapper>
+      <CapabilityBoxContainer>
+        {CapabilityBoxes.map(({ title, content, iconSrc }) => (
+          <CapabilityBox
+            title={title}
+            content={content}
+            iconSrc={iconSrc}
+            key={title}
+          />
+        ))}
+      </CapabilityBoxContainer>
+    </CapabilityBoxContainerWrapper>
   </Root>
 );
 
