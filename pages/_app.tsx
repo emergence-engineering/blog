b634ae24fdf83837141aedcbbfb2725d7a6e5276
Norbert-EE <norbert@emergence-engineering.com>
2024-09-11 10:26:31 +0200

    check fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,15 @@ import Head from "next/head";
 import "../public/global.css";
 import "prosemirror-slash-menu-react/dist/styles/menu-style.css";
 import { Provider as UniqueIdGeneratorProvider } from "@inline-svg-unique-id/react";
+import { PT_Sans_Narrow } from "next/font/google";
 
 interface MyAppProps extends AppProps {}
+
+const ptSansNarrow = PT_Sans_Narrow({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 class MyApp extends App<MyAppProps> {
   render() {
@@ -20,24 +27,14 @@ class MyApp extends App<MyAppProps> {
             href="/ee-icon-4848.png"
           />
           <link rel="apple-touch-icon" href="/ee-icon-192192.png" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=PT+Sans+Narrow:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
 
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         {/* @ts-ignore */}
         <UniqueIdGeneratorProvider>
-          <Component {...pageProps} />
+          <div className={ptSansNarrow.className}>
+            <Component {...pageProps} />
+          </div>
         </UniqueIdGeneratorProvider>
       </>
     );
